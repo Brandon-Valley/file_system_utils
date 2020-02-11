2876e8e7956cee5276ceec55900c6c76d99ea366
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-11 15:03:44 -0600

    added ability to relative import

--- file_system_utils.py
+++ file_system_utils.py
@@ -1,3 +1,12 @@
+''' [======- - - - -=================- All Utilities Standard -=================- - - - -======] '''
+# to allow for relative imports
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__))))
+''' [======- - - - - - -=============- - - - -========- - - - -=============- - - - - - -======] '''
+
+
+
+
 import glob
 import os
 # from shutil import copyfile
